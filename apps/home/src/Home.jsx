58b95f0c55fac6e7374a92abffe0e73e873361e3
avishektiwari <avishektiwari@lftechnology.com>
2024-03-03 18:18:55 +0545

    error handling

--- apps/home/src/Home.jsx
+++ apps/home/src/Home.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import Details from "details/Details";
+import SafeComponent from "./SafeComponent";
 
 const Home = (props) => {
   const [viewDetails, setViewDetails] = useState(false);
@@ -13,7 +14,11 @@ const Home = (props) => {
         This is the main section. Click the button bellow to view the details.
       </div>
       <button onClick={() => onClick()}>Details</button>
-      {viewDetails && <Details />}
+      {viewDetails && (
+        <SafeComponent>
+          <Details />
+        </SafeComponent>
+      )}
     </div>
   );
 };
